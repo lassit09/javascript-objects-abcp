d00df1ef40decc3970e8c0f13e743a375e15cf98
Taylor Lassiter <lassit09@gmail.com>
2018-01-11 04:37:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  return Object.assign(playlist, {})
+  return Object.assign(delete playlist.artistName);
 }
