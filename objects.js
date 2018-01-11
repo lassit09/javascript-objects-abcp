f19520a2959de14a099633aa7acacfa2521bda17
Taylor Lassiter <lassit09@gmail.com>
2018-01-11 04:21:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = new Object({Phish: 'Harry Hood'});
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, {[artistName]: songTitle});
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist.artistName;
+}
